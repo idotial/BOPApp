0887f65cfc6767ebaded3fb5b472f4d17acad1e3
homeMac <724104612@qq.com>
2018-06-01 18:59:43 +0800

    import类型显示

--- src/component/ImportingAccount.js
+++ src/component/ImportingAccount.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {
   Button,
   Modal,
+  Picker,
   StyleSheet,
   Text,
   TextInput,
@@ -16,12 +17,14 @@ type Props = {
 }
 
 type State = {
+  importType:string,
   password: string,
   data:string,
 }
 
 export default class ImportAccount extends Component<Props, State> {
   state = {
+    importType: '0',
     password: '341314341241234',
     data: 'wwwww',
   }
@@ -34,14 +37,38 @@ export default class ImportAccount extends Component<Props, State> {
     console.log('ImportAccountModal componentWillUnmount');
   }
 
-
+  importTypeString = () => {
+    var typeName = ''
+    switch (this.state.importType) {
+      case '1':
+        typeName = 'keystore导入'
+        break;
+      case '2':
+        typeName = '私钥导入'
+        break;
+      default:
+        typeName = '助记词导入'
+    }
+    return typeName
+  }
 
   render() {
     return (
-        <View>
-          <Text style={styles.title}>导入账号</Text>
-          <TextInput multiline={true} value={this.state.password} onChangeText={(password) => {this.setState(password)}} />
-          <TextInput multiline={true} value={this.state.data} onChangeText={(data) => {this.setState(data)}} />
+        <View style={styles.container}>
+          <View>
+            <TextInput style={styles.title} pw={this.state.importType} value={this.importTypeString()} />
+            <TextInput multiline={true} value={this.state.password} onChangeText={(password) => {this.setState(password)}} />
+            <TextInput multiline={true} value={this.state.data} onChangeText={(data) => {this.setState(data)}} />
+            <Picker
+              selectedValue={this.state.importType}
+              style={{ height: 50}}
+              onValueChange={(itemValue, itemIndex) => this.setState({importType: itemValue})}>
+              <Picker.Item label="助记词导入" value="0" />
+              <Picker.Item label="keystore导入" value='1' />
+              <Picker.Item label="私钥导入" value="2" />
+            </Picker>
+          </View>
+
           <View style={styles.buttons}>
             <Button title='确认' onPress={this.props.importDidSuccess} />
             <Button title='取消' onPress={this.props.importCancel} />
@@ -54,6 +81,7 @@ export default class ImportAccount extends Component<Props, State> {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
     backgroundColor: '#240bed',
   },
   input:{
@@ -68,6 +96,6 @@ const styles = StyleSheet.create({
   buttons: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginTop: 20,
+    marginTop: 400,
   },
 })
